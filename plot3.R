bb72a16adfa89cc3e6b9dad35f850ca3891d0b08
Oliver Kliegel <oliver.kliegel@gmail.com>
2014-07-12 10:49:36 +0200

    Initial attempt

---
+++ plot3.R
@@ -0,0 +1,18 @@
+EPC <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?")
+EPC$DateTime <- paste(EPC$Date,EPC$Time)
+EPC$DateTime <- strptime(EPC$DateTime, "%d/%m/%Y %H:%M:%S")
+EPC<-(EPC[,3:10])
+StartDate <- as.Date("2007/02/01 0:0:0", format ="%Y/%m/%d %H:%M:%S")
+EndDate <- as.Date("2007/02/02 23:59:59", format ="%Y/%m/%d %H:%M:%S")
+EPC <- EPC[as.Date(EPC$DateTime, format ="%Y/%m/%d %H:%M:%S") >= as.Date(StartDate),]
+EPC <- EPC[as.Date(EPC$DateTime, format ="%Y/%m/%d %H:%M:%S") <= as.Date(EndDate),]
+
+png(file="plot3.png")
+par(mfrow=c(1,1))
+plot(EPC$Sub_metering_1, xlab="", ylab="Energiy sub metering",type ="n",xaxt = "n")
+points(EPC$Sub_metering_1, type="l")
+points(EPC$Sub_metering_2, type="l", col="red")
+points(EPC$Sub_metering_3, type="l", col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=c(1,1))
+axis(side = 1, c(0,1450,2900),labels=c("Thu","Fri","Sat"))
+dev.off()
